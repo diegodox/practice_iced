d8169c8519c0cefbd9235fafe03d46827213663e
Diego Fujii <android.mxdiego9@gmail.com>
2020-07-11 16:39:45 +0900

    github pages

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 cd C:\Users\mxdie\Documents\研究\practice_iced
-C:\Users\mxdie\Documents\CLI-Tools\wasm-bindgen-0.2.64-x86_64-pc-windows-msvc\wasm-bindgen.exe ./target/wasm32-unknown-unknown/debug/gui-test.wasm --out-dir out --web
+C:\Users\mxdie\Documents\CLI-Tools\wasm-bindgen-0.2.64-x86_64-pc-windows-msvc\wasm-bindgen.exe ./target/wasm32-unknown-unknown/debug/gui-test.wasm --out-dir /docs/out --web
